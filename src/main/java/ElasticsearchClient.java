5e342658d439d7441d5746147c726671a8499f17
suhyunjeon <suhyunjun@gmail.com>
2015-12-07 00:05:16 +0900

    목업 코드

---
+++ src/main/java/ElasticsearchClient.java
@@ -0,0 +1,72 @@
+import com.google.gson.Gson;
+import io.searchbox.client.JestResult;
+import io.searchbox.core.*;
+import io.searchbox.client.JestClient;
+import io.searchbox.client.JestClientFactory;
+import io.searchbox.client.config.HttpClientConfig;
+import io.searchbox.indices.CreateIndex;
+
+import java.io.IOException;
+
+/**
+ * Jest client 를 이용하여 elasticsearch 에 데이터를 저장한다.
+ * Mockup 코드
+ *
+ * User: suhyunjeon
+ * Date: 2015. 12. 6.
+ */
+public class ElasticsearchClient {
+
+    public static void main(String args[]) throws IOException {
+        JestClient client = jestClient();
+
+        createIndex(client);
+        indexDocument(client);
+        getDocument(client);
+
+        search(client);
+    }
+
+    private static void getDocument(JestClient client) throws IOException {
+        Get get = new Get.Builder("diet", "1").type("dieter").build();
+
+        JestResult result = client.execute(get);
+        System.out.println(result.getJsonObject());
+        System.out.println(result.getValue("_source"));
+    }
+
+    private static void search(JestClient client) throws IOException {
+        Search search = new Search.Builder("").addIndex("diet").addType("dieter").build();
+        SearchResult result = client.execute(search);
+        System.out.println(result.getTotal());
+    }
+
+    private static void createIndex(JestClient client) throws IOException {
+        CreateIndex createIndex = new CreateIndex.Builder("diet").build();
+        client.execute(createIndex);
+
+        System.out.println(createIndex.getURI());
+        System.out.println(createIndex.getRestMethodName());
+        String settings = new Gson().toJson(createIndex.getData(new Gson()));
+        System.out.println(settings);
+    }
+
+    private static void indexDocument(JestClient client) throws IOException {
+        String source = "{\"user\":\"tasha\", \"plan\":\"no food\"}";
+
+        Index index = new Index.Builder(source).index("diet").type("dieter").id("1").build();
+        client.execute(index);
+
+        System.out.println(index.getRestMethodName());
+        System.out.println(index.getURI());
+    }
+
+    private static JestClient jestClient() {
+        JestClientFactory factory = new JestClientFactory();
+        factory.setHttpClientConfig(new HttpClientConfig
+                .Builder("http://localhost:9200")
+                .multiThreaded(true)
+                .build());
+        return factory.getObject();
+    }
+}
