a54f144f35013fd9e1d8970c748b07c15a70a9a8
Su Hyun Jeon <suhyunjun@gmail.com>
2015-12-07 00:21:08 +0900

    Update ElasticsearchClient.java

--- src/main/java/ElasticsearchClient.java
+++ src/main/java/ElasticsearchClient.java
@@ -5,6 +5,8 @@ import io.searchbox.client.JestClient;
 import io.searchbox.client.JestClientFactory;
 import io.searchbox.client.config.HttpClientConfig;
 import io.searchbox.indices.CreateIndex;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
@@ -16,6 +18,8 @@ import java.io.IOException;
  * Date: 2015. 12. 6.
  */
 public class ElasticsearchClient {
+    
+    final static Logger logger = LoggerFactory.getLogger(ElasticsearchClient.class);
 
     public static void main(String args[]) throws IOException {
         JestClient client = jestClient();
@@ -23,42 +27,40 @@ public class ElasticsearchClient {
         createIndex(client);
         indexDocument(client);
         getDocument(client);
-
-        search(client);
+//        search(client);
     }
 
     private static void getDocument(JestClient client) throws IOException {
         Get get = new Get.Builder("diet", "1").type("dieter").build();
-
         JestResult result = client.execute(get);
-        System.out.println(result.getJsonObject());
-        System.out.println(result.getValue("_source"));
+
+        logger.debug(result.getJsonObject().toString());
+        logger.debug(result.getValue("_source").toString());
     }
 
     private static void search(JestClient client) throws IOException {
         Search search = new Search.Builder("").addIndex("diet").addType("dieter").build();
         SearchResult result = client.execute(search);
-        System.out.println(result.getTotal());
+        logger.debug(result.getTotal().toString());
     }
 
     private static void createIndex(JestClient client) throws IOException {
         CreateIndex createIndex = new CreateIndex.Builder("diet").build();
         client.execute(createIndex);
 
-        System.out.println(createIndex.getURI());
-        System.out.println(createIndex.getRestMethodName());
-        String settings = new Gson().toJson(createIndex.getData(new Gson()));
-        System.out.println(settings);
+        logger.debug(createIndex.getURI());
+        logger.debug(createIndex.getRestMethodName());
+        
+        logger.debug(new Gson().toJson(createIndex.getData(new Gson())));
     }
 
     private static void indexDocument(JestClient client) throws IOException {
         String source = "{\"user\":\"tasha\", \"plan\":\"no food\"}";
-
         Index index = new Index.Builder(source).index("diet").type("dieter").id("1").build();
         client.execute(index);
 
-        System.out.println(index.getRestMethodName());
-        System.out.println(index.getURI());
+        logger.debug(index.getRestMethodName());
+        logger.debug(index.getURI());
     }
 
     private static JestClient jestClient() {
