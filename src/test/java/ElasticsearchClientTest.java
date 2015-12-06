148c6e70a6112ae9bd9952a3b585a71fd77d32c8
suhyunjeon <suhyunjun@gmail.com>
2015-12-07 00:05:27 +0900

    목업 코드

---
+++ src/test/java/ElasticsearchClientTest.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: suhyunjeon
+ * Date: 2015. 12. 6.
+ * Time: 오후 10:20
+ * To change this template use File | Settings | File Templates.
+ */
+public class ElasticsearchClientTest {
+}
